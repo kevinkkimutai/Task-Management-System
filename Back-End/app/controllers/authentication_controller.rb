0d234e50890f825c9b75a479da0f4593e62ffd30
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-21 19:30:04 +0300

    auth errror solved

--- Back-End/app/controllers/authentication_controller.rb
+++ Back-End/app/controllers/authentication_controller.rb
@@ -1,23 +1,12 @@
 class AuthenticationController < ApplicationController
     skip_before_action :authorize_request, only: :login
     def login
-      user = User.find_by(email: params[:email])
-  
-      if user && user.authenticate(params[:password])
-        # Redirect to appropriate dashboard based on user role
-        if user.admin?
-          user.update(role: 'admin')
-          # Login as admin
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
           token = JWT.encode({ user_id: user.id }, "secret")
-          render json: { message: "Logged in successfully as admin", user: user, token: token, role: user.role }
+          render json: {message: "loggedin successfully",user: user, token: token }
         else
-          user.update(role: 'student')
-          # Login as student
-          token = JWT.encode({ user_id: user.id }, "secret")
-          render json: { message: "Logged in successfully as student", user: user, token: token }
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
-      else
-        render json: { error: 'Invalid email or password' }, status: :unauthorized
       end
-    end
 end
