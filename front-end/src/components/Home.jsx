f54ea7ddebc5360273a4f454971a0f8a42df4069
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-21 22:05:13 +0300

    readme updated

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -7,6 +7,7 @@ function Home() {
       <div className="col-5">
       This is a simple web application created using reactjs and bootstrap that allows users to manage their tasks effectively. With this application, users can create new tasks, mark them as complete,
        and delete tasks when they are no longer needed.
+      <p>Backend Link: <a href="https://task-management-wm42.onrender.com">https://task-management-wm42.onrender.com</a></p>
     </div>
     <div className="col-5">
     <img className="img-fluid" src={task} alt="profile" />
