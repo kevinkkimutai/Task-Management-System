7ac4d2c52a6dc374bee4716a9ef3f8bebc97c5da
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-21 20:48:23 +0300

    paddings fixed

--- front-end/src/components/Tasks.jsx
+++ front-end/src/components/Tasks.jsx
@@ -139,7 +139,7 @@ function Tasks({ token, user }) {
      {dueTodayTasks.map((task) => (
                   <li key={task.id} className={`lss ${task.status === 'COMPLETED' ? 'done-list' : ''}`}>
                     {task.name}
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" type="button" onClick={() => deleteTask(task.id)} className="bi bi-trash trash me-3" viewBox="0 0 16 16">
+<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" type="button" onClick={() => deleteTask(task.id)} className="bi bi-trash trash me-3 pt-1" viewBox="0 0 16 16">
   <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
   <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
 </svg>
@@ -200,7 +200,7 @@ function Tasks({ token, user }) {
               <div className="p-1">
                 <div
                   className={`card ${task.status === 'COMPLETED' ? 'done-card' : ''}`}
-                  style={{ height: '13rem', width: '17rem' }}
+                  style={{ height: '13rem', width: '16.9rem' }}
                 >
                   <div className="card-body">
                     <h5 className="card-title">{task.name}</h5>
