badd6df302ac3302f6a699351c63c32ab9f2aeb5
kevinkkimutai <kiruikevinkim@gmail.com>
2023-06-21 21:16:00 +0300

    paddings fixed

--- Back-End/app/controllers/users_controller.rb
+++ Back-End/app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      token = JWT.encode({ user_id: user.id }, Rails.application.secrets.secret_key_base)
+      token = JWT.encode({ user_id: user.id }, "secret")
       render json: { user: user, token: token }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
